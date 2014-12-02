f56cc1d476db95142a3a4ad3af64baba0cc1db05
Alathea <alathea@zoology.ubc.ca>
2014-12-01 16:36:25 -0800

    updated documentation

--- captioner/R/captioner.R
+++ captioner/R/captioner.R
@@ -1,5 +1,9 @@
 #' captioner: A package for numbering figures and generating captions
 #'
+#' Contains the functions \code{\link{caption}} for generating captions
+#' and \code{\link{incrementer()}} for generating figure or table
+#' numbers.
+#'
 #' @author Alathea D Letaw, \email{alathea@@zoology.ubc.ca}
 #' 
 #' @docType package
