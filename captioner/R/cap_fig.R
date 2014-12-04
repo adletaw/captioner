fc44f14130431b979d0d783efeecbe57be51100d
Alathea <alathea@zoology.ubc.ca>
2014-12-03 21:59:52 -0800

    filled out cap_fig and cap_tab functions

--- captioner/R/cap_fig.R
+++ captioner/R/cap_fig.R
@@ -4,16 +4,30 @@
 #' 
 #' @examples my_caption <- cap_fig("flower_plot", "Histogram of flower colors.")
 #' 
-#' @seealso \code{\link{captioner}}, \code{\link{cap_tab}}
+#' @seealso \code{\link{captioner}}
 #' 
 #' @export
 
 cap_fig <- function(name, cite = FALSE, caption = "")
 {
-  captioner(prefix = "Figure")
+  if(exists("FIG_CAPTION_COUNTER", envir = globalenv())){
+    assign("FIG_CAPTION_COUNTER", FIG_CAPTION_COUNTER + 1, envir = globalenv())
+  } else{
+    assign("FIG_CAPTION_COUNTER", 1, envir = globalenv())
+    caption_it <- captioner(prefix = "Figure")
+  }
+  
+  caption_it(name, cite, caption)
 }
 
 cap_tab <- function(name, cite = FALSE, caption = "")
 {
-  captioner(prefix = "Table")
+  if(exists("TAB_CAPTION_COUNTER", envir = globalenv())){
+    assign("TAB_CAPTION_COUNTER", TAB_CAPTION_COUNTER + 1, envir = globalenv())
+  } else{
+    assign("TAB_CAPTION_COUNTER", 1, envir = globalenv())
+    caption_it <- captioner(prefix = "Table")
+  }
+  
+  caption_it(name, cite, caption)
 }
