5123b44309a875bdb968c2d512f0a0daf63a4bdb
Alathea <alathea@zoology.ubc.ca>
2014-12-03 21:24:55 -0800

    combined cap_tab and cap_fig as they do the same thing

--- captioner/R/cap_fig.R
+++ captioner/R/cap_fig.R
@@ -1,10 +1,19 @@
-#' Figure caption generator
+#' Simple figure/table caption generators
 #' 
-#' Allows you to create a table caption with automatically generated numbered prefix.
+#' A simplified captioner to create numbered figure or table captions without generating your own captioner
 #' 
 #' @examples my_caption <- cap_fig("flower_plot", "Histogram of flower colors.")
 #' 
 #' @seealso \code{\link{captioner}}, \code{\link{cap_tab}}
 #' 
 #' @export
-cap_fig <- captioner(prefix = "Figure")
+
+cap_fig <- function(name, cite = FALSE, caption = "")
+{
+  captioner(prefix = "Figure")
+}
+
+cap_tab <- function(name, cite = FALSE, caption = "")
+{
+  captioner(prefix = "Table")
+}
