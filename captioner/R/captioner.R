3107c003c230520d7dd1bb1fd40875ee6467fc47
Alathea <alathea@zoology.ubc.ca>
2015-01-07 13:47:36 -0800

    added vector to store captions

--- captioner/R/captioner.R
+++ captioner/R/captioner.R
@@ -39,25 +39,32 @@
 captioner <- function(prefix = "Figure")
 {
   OBJ_LIST <- c()
+  CAPTIONS <- c()
   force(prefix)
   
   function(name, caption = "", cite = FALSE)
   {
-    # grab the object vector from the enclosing environment
+    # grab the object and caption vectors from the enclosing environment
     obj_list <- OBJ_LIST
+    captions <- CAPTIONS
     
     # check to see if the object name is already stored
-    if(!any(OBJ_LIST == name)) {
+    if(any(obj_list == name)) {
+      obj_num <- match(name, obj_list)
+      text <- captions[obj_num]
+    } else {
       # add the ojbect to the local copy of the object vector
       obj_list <- c(obj_list, name)
+      captions <- c(captions, caption)
       
-      # assign the local object vector to the copy in the enclosing environment
+      # get the number of the object
+      obj_num <- match(name, obj_list)
+      
+      # assign the local vector to the copy in the enclosing environment
       assign("OBJ_LIST",  obj_list,  envir = parent.env(environment()))
+      assign("CAPTIONS",  captions,  envir = parent.env(environment()))
     }
         
-    # get the number of the object
-    obj_num <- match(name, obj_list)
-    
     # choose between short or long format
     if(cite) { text <- paste0(prefix, " ", obj_num) }
     else     { text <- paste0(prefix, " ", obj_num, ": ", caption) }
