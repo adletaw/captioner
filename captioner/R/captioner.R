f64547a6e37ba88de08f361a2b9c87e73c12dc64
Alathea <alathea@zoology.ubc.ca>
2015-01-30 10:45:22 -0800

    set default infix to NULL

--- captioner/R/captioner.R
+++ captioner/R/captioner.R
@@ -41,7 +41,7 @@
 #' @export
 
 captioner <- function(prefix = "Figure", auto_space = TRUE, levels = 1,
-                      type, infix = ".")
+                      type = NULL, infix = ".")
 {
   # Check the parameter classes
   check_class(prefix,     "character")
