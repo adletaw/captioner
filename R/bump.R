63617b37dbb09b5029522d907732e3dd1afa4a72
Alathea <alathea@zoology.ubc.ca>
2015-02-10 14:32:47 -0800

    dont test examples

--- R/bump.R
+++ R/bump.R
@@ -14,12 +14,14 @@
 #' to shift from Fig 1.5 to Fig 2.1. 
 #' 
 #' @examples
-#' Create a new captioner object:
+#' \donttest{
+#' # Create a new captioner object:
 #' 
 #' figs <- captioner(levels = 2)
 #' figs("a")
 #' 
 #' bump(figs, 1)
+#' }
 #' 
 #' @seealso \code{\link{captioner}}
 #' 
