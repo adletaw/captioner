4faf1b06cf24d5f7d5e849f85a4d3c090848fa6f
Alathea <alathea@zoology.ubc.ca>
2016-02-12 12:32:57 -0800

    changed to an internal function (#6)

--- R/cap_knitr-internal.R
+++ R/cap_knitr-internal.R
@@ -2,24 +2,17 @@
 #'
 #' Option to automatically show a caption after a knitr chunk.
 #'
-#' @param cap_function The name of the captioner function to be used, in quotes.
+#' @param before Logical determining whether to display the caption before
+#' or after the figure.
 #' 
 #' @return NULL
 #' 
-#' @details cap_knitr sets a knitr option so your captions will automatically
-#' display.  The caption is found by accessing the chunk name, so the caption
-#' and chunk names must match.
-#' 
-#' To accomodate multiple captioner functions, cap_knitr uses the name of the
-#' function you specify to generate the option name.  In other words, if you
-#' have a captioner function called \code{fig_caps}, the knitr option will be
-#' "fig_caps".  This option is logical (TRUE/FALSE).
-#' 
-#' @examples
-#' 
-#' figs <- captioner()
-#' cap_knitr("figs")
-#'   
+#' @details cap_knitr sets the knitr option \code{cap} so your captions will
+#' automatically display.  The caption is found by accessing the chunk name,
+#' so the caption and chunk names must match. \code{cap} takes the name of
+#' the corresponding captioner function, in quotes.
+#'
+#' @keywords internal   
 #' @export
 
 ## This should be an internal function that is automatically called when
