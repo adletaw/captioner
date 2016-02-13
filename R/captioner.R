271f0c414d2d5ab27ef0682f57a966da081faee7
Alathea <alathea@zoology.ubc.ca>
2016-02-13 10:41:28 -0800

    auto_space fix

--- R/captioner.R
+++ R/captioner.R
@@ -88,7 +88,7 @@ captioner <- function(prefix = "Figure", auto_space = TRUE, levels = 1,
   
   # Add a space after the prefix if auto_space is on
   if(auto_space){
-    prefix <- paste(prefix, " ")
+    prefix <- paste(prefix, " ", sep = "")
   }
   
   # Force the parameter values for use in the return function
